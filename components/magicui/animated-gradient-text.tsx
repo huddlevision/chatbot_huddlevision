20d6515b4f752cd1dfce6379da28d4c9f54f4edb
Ben <ben@fantasyfootballdatapros.com>
2024-06-13 13:22:46 -0400

    lets try this

--- components/magicui/animated-gradient-text.tsx
+++ components/magicui/animated-gradient-text.tsx
@@ -16,7 +16,7 @@ export default function AnimatedGradientText({
       )}
     >
       <div
-        className={`absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#0EA5E9]/50 via-[purple]/50 to-[blue]/50 bg-[length:var(--bg-size)_100%] p-[1px] ![mask-composite:subtract] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`}
+        className={`absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#0EA5E9]/50 via-[purple]/50 to-[blue]/50 bg-[length:var(--bg-size)_100%] p-[1px] ![mask-composite:subtract] rounded-md [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`}
       />
 
       {children}
